6eba5056bddbc199d1681aa679a486695a54b829
wangalrebt <wangalrebt@gmail.com>
2012-12-08 12:46:26 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blue::Application.config.secret_token = '8cb1b05af1f0155064b9116a04c6fb0522f466078a16baa7ed3f888e9e15b89b81290345401b484daa7f9d2bb0004ce536ea29b59df40d669633b5d11cd56867'
