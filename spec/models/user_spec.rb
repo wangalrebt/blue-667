df297c12ceadac5e8b11c56265e390443ba3bfdb
wangalrebt <wangalrebt@gmail.com>
2013-05-26 19:44:06 -0400

    Add Rspec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,57 @@ describe User do
   end
 
   it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
   end
+
+  it "should accept valid email addresses" do
+    addressess = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addressess.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo. THE_USER_at_foo.bar first.last@foo,jp]
+    addresses.each do |address|
+      in_valid_email_user = User.new(@attr.merge(:email => address))
+      in_valid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  it "should reject email address identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+
 end
